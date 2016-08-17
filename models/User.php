460bd722162dd4282ced529e2a635754486cd2b7
James Pakorn <jamespakorn@Jamess-MacBook-Pro.local>
2016-08-17 12:03:27 +0700

    test model

--- models/User.php
+++ models/User.php
@@ -9,6 +9,7 @@ class User extends \yii\base\Object implements \yii\web\IdentityInterface
     public $password;
     public $authKey;
     public $accessToken;
+    public $test;
 
     private static $users = [
         '100' => [
